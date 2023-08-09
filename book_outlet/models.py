606391ea5842c38aef111e76855b0251c72e6b75
SifatNiloy <sifatniloy18@gmail.com>
2023-08-09 23:35:35 +0600

    updated models and migrated

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,8 +5,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title= models.CharField(max_length=50)
     rating= models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author= models.CharField(max_length=100)
-    is_bestselling= models.BooleanField()
+    author= models.CharField(null=True,max_length=100)
+    is_bestselling= models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title} {self.rating}"
